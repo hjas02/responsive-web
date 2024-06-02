3520720858f2a0ed703dc2fe94a157d6d901930b
hjas02 <hyeonho9509@naver.com>
2024-06-02 13:08:49 +0900

    수정

--- HW/HW13/main.js
+++ HW/HW13/main.js
@@ -11,6 +11,24 @@ var y3 = centerY + a;
 var color = "yellow";
 var rotationAngle = 0; 
 
+function sign(p1, p2, p3) {
+    return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);
+}
+
+function pointInTriangle(pt, v1, v2, v3) {
+    var d1, d2, d3;
+    var has_neg, has_pos;
+
+    d1 = sign(pt, v1, v2);
+    d2 = sign(pt, v2, v3);
+    d3 = sign(pt, v3, v1);
+
+    has_neg = (d1 < 0) || (d2 < 0) || (d3 < 0);
+    has_pos = (d1 > 0) || (d2 > 0) || (d3 > 0);
+
+    return !(has_neg && has_pos);
+}
+
 function drawTriangle() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -40,13 +58,12 @@ canvas.addEventListener('click', function (event) {
     var clickX = event.offsetX;
     var clickY = event.offsetY;
 
-    var b = x2 - x3 
-    var height =y2 - centerY; 
-    
-    var A = 0.5 * b * height; // 삼각형의 넓이
-    var s = (centerY * x3 - centerX * y3 + (y3 - centerY) * clickX + (centerX - x3) * clickY) * -1;
-    var t = (centerX * y2 - centerY * x2 + (centerY - y2) * clickX + (x2 - centerX) * clickY) * -1;
-    if (s > 0 && t > 0 && (s + t) < 2 * A * -1) {
+    var pt = {x: clickX, y: clickY};
+    var v1 = {x: centerX, y: centerY};
+    var v2 = {x: x2, y: y2};
+    var v3 = {x: x3, y: y3};
+
+    if (pointInTriangle(pt, v1, v2, v3)) {
         color = "red";
     } else {
         color = "yellow";
